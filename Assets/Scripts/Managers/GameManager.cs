a6cbdbcc132abff3a9a9b25ee9d1bacdae81cd92
Irtaza Khan <irtazakhan100@gmail.com>
2023-06-30 22:30:29 +0200

    Remove unnecessary stuff

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -17,21 +17,6 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         Instance_UI = FindAnyObjectByType<UIManager>();
-        //if (File.Exists(Application.dataPath + "/words.txt"))
-        //{
-        //    string json = File.ReadAllText(Application.dataPath + "/words.txt");
-        //    wordsList = JsonUtility.FromJson<WordsList>(json);
-        //}
-
-        //int numGameSession = FindObjectsOfType<GameManager>().Length;
-        //if (numGameSession > 1)
-        //{
-        //    Destroy(gameObject);
-        //}
-        //else
-        //{
-        //    DontDestroyOnLoad(gameObject);
-        //}
 
         if (Instance != null)
         {
@@ -45,24 +30,3 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 }
-
-//[System.Serializable]
-//public class WordsList
-//{
-//    public List<WordData> wordData = new List<WordData>();
-//}
-
-//[System.Serializable]
-//public class WordData
-//{
-//    public int No;
-//    public string EN_Name;
-//    public string Gender;
-//    public string SP_Name;
-//    public int Length;
-//    public string Type;
-//    public string EN_Definition;
-//    public string SP_Definition;
-//    public string Sprite;
-//    public string Audio;
-//}
