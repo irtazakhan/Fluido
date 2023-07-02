c32da946a1834aa744c349986cdcb20a5d32b6dd
Irtaza Khan <irtazakhan100@gmail.com>
2023-07-02 16:26:49 +0200

    Update TTS

--- Assets/Scripts/TextToSpeech.cs
+++ Assets/Scripts/TextToSpeech.cs
@@ -7,10 +7,10 @@ public class TextToSpeech : MonoBehaviour
     private const string TTS_URL = "https://api.voicerss.org/";
 
     private const string apiKey = "dd857ab0fd534ac7b49ddf84bbc47128"; // Replace with your API key from VoiceRSS
-    private string lang_Eng = "en-us"; // Language code: en-us for English,
+    private string lang_Eng = "en-us";  // Language code: en-us for English,
     private string lang_Spa = "es-es"; // Language code: es-es for Spanish
-    public float pitch = 0.5f; // Speech pitch (0.0 to 1.0)
-    public float rate = -10f; // Speech rate (0.0 to 1.0)
+    private string voice = ""; // Voice Speaker Name
+    [Range(-10f, 10f)] public float rate = 0f; // Speech rate (-10.0 to 10.0)
 
     public void Speak(string text)
     {
@@ -19,7 +19,7 @@ public class TextToSpeech : MonoBehaviour
 
     private IEnumerator DownloadSpeech(string text)
     {
-        string url = $"{TTS_URL}?key={apiKey}&hl={lang_Spa}&c=MP3&f=16khz_16bit_stereo&src={UnityWebRequest.EscapeURL(text)}&r={rate}&v={pitch}";
+        string url = $"{TTS_URL}?key={apiKey}&hl={lang_Spa}&c=MP3&f=16khz_16bit_stereo&src={UnityWebRequest.EscapeURL(text)}&r={rate}&v={voice}";
 
         using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG))
         {
