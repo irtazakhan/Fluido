6b153957c70f6075e60f314b27e675f877e4aba8
Irtaza Khan <irtazakhan100@gmail.com>
2023-07-06 18:35:49 +0200

    Added Voices and Options to TTS

--- Assets/Scripts/TextToSpeech.cs
+++ Assets/Scripts/TextToSpeech.cs
@@ -7,10 +7,33 @@ public class TextToSpeech : MonoBehaviour
     private const string TTS_URL = "https://api.voicerss.org/";
 
     private const string apiKey = "dd857ab0fd534ac7b49ddf84bbc47128"; // Replace with your API key from VoiceRSS
-    private string lang_Eng = "en-us";  // Language code: en-us for English,
-    private string lang_Spa = "es-es"; // Language code: es-es for Spanish
-    private string voice = ""; // Voice Speaker Name
+  //  private string lang_Eng = "en-us";  // Language code: en-us for English,
+  //  private string lang_Spa = "es-es"; // Language code: es-es for Spanish
+  //  private string voice = ""; // Voice Speaker Name
     [Range(-10f, 10f)] public float rate = 0f; // Speech rate (-10.0 to 10.0)
+    public SpanishAccent selectedLanguage;
+    public MexicoVoice voice_Mexico;
+    public SpainVoice voic_Spain;
+    public enum SpanishAccent
+    {
+        Spain,
+        Mexico
+    }
+    public enum MexicoVoice
+    {
+        Juana,
+        Silvia,
+        Teresa,
+        Jose
+    }
+
+    public enum SpainVoice
+    {
+        Camila,
+        Sofia,
+        Luna,
+        Diego
+    }
 
     public void Speak(string text)
     {
@@ -19,7 +42,9 @@ public class TextToSpeech : MonoBehaviour
 
     private IEnumerator DownloadSpeech(string text)
     {
-        string url = $"{TTS_URL}?key={apiKey}&hl={lang_Spa}&c=MP3&f=16khz_16bit_stereo&src={UnityWebRequest.EscapeURL(text)}&r={rate}&v={voice}";
+        string languageCode = GetLanguageCode(selectedLanguage);
+        string voice = (selectedLanguage == SpanishAccent.Mexico) ? voice_Mexico.ToString() : voic_Spain.ToString();
+        string url = $"{TTS_URL}?key={apiKey}&hl={languageCode}&c=MP3&f=16khz_16bit_stereo&src={UnityWebRequest.EscapeURL(text)}&r={rate}&v={voice}";
 
         using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(url, AudioType.MPEG))
         {
@@ -36,6 +61,18 @@ public class TextToSpeech : MonoBehaviour
                 audioSource.clip = clip;
                 audioSource.Play();
             }
+        }
+    }
+    private string GetLanguageCode(SpanishAccent language)
+    {
+        switch (language)
+        {
+            case SpanishAccent.Mexico:
+                return "es-mx"; // English language code
+            case SpanishAccent.Spain:
+                return "es-es"; // Spanish language code
+            default:
+                return "es-es"; // Default language code
         }
     }
 }
