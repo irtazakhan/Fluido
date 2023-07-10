e6d1821803849fbff98db12204f02e72cd188d46
Irtaza Khan <irtazakhan100@gmail.com>
2023-07-11 01:17:45 +0200

    Analyze Panel Changes

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -18,6 +18,7 @@ public class LevelLoader : MonoBehaviour
     }
     public void LoadGame()
     {
+        SoundManager.ins.PlaySfx("Atacar");
         StartCoroutine(ColorChangeRoutine());
     }
 
