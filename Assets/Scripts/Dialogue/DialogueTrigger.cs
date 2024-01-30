0f0411b625a27422f1efaf0e060d98838d1407c9
DIkshuj97 <dikshumittal44@gmail.com>
2024-01-30 06:59:34 +0100

    Added Tutorial Dialogue

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -8,9 +8,23 @@ public class DialogueTrigger : MonoBehaviour
     public Dialogue dialogue;
     public DialogueManager dialogueManager;
 
+    public bool startDialogue;
     private void Start()
     {
-        dialogueManager.StartDialogue(dialogue,false);
+        if(startDialogue)
+        {
+            dialogueManager.StartDialogue(dialogue, false);
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            dialogueManager.StartDialogue(dialogue, false);
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 
 }
