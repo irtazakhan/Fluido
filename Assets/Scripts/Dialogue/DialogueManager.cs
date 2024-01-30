552753d1e08140f3434dd70cac680a78b369e407
DIkshuj97 <dikshumittal44@gmail.com>
2024-01-30 07:09:12 +0100

    Minor Bug Fixes

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -81,7 +81,7 @@ public class DialogueManager : MonoBehaviour
 		foreach (char letter in sentence.ToCharArray())
 		{			
 			dialogueText.text += letter;
-			yield return new WaitForSeconds(0.09f);
+			yield return new WaitForSeconds(0.07f);
 		}
 		isSentenceEnded = true;
 	}
